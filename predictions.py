3a44712c68f257066b01af1887dea461b208599b
jbuklis1 <112995577+jbuklis1@users.noreply.github.com>
2025-03-11 23:48:17 -0500

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -25,19 +25,19 @@ print("Default")
 print(h.feature_importances_)
 preds = h.predict(Xtest)
 print(mean_squared_error(Ytest, preds))
-
+print(mean_absolute_error(Ytest, preds))
 rep = input("Enter new hyperparameters y/n\n")
 while (not "n" in rep):
 	depth = int(input("Max Depth (int): "))
-	if (depth == 0): depth = 'None'
+	if (depth == 0): depth = None
 	minSamplesLeaf = float(input("Min Samples Leaf (float between 0 and 1): "))
 	if (minSamplesLeaf == 0.0): minSamplesLeaf = 0
 	elif (minSamplesLeaf == 1.0): minSamplesLeaf = 1
-
 	modelCur = tree.DecisionTreeRegressor(max_depth=depth, min_samples_leaf=minSamplesLeaf)
 	modelCur.fit(X,Y)
-	print("Feature Importances for 'HomeAway', 'Opp', 'Season', 'Rest': " + str(modelCur.feature_importances_))
 	preds = modelCur.predict(Xtest)
+
+	print("Feature Importances for 'HomeAway', 'Opp', 'Season', 'Rest': " + str(modelCur.feature_importances_))
 	print("Mean Squared Error: " + str(mean_squared_error(Ytest, preds)))
 	print("Mean Absolute Error: " + str(mean_absolute_error(Ytest, preds)))
 	rep = input("\nEnter new hyperparameters y/n\n")
